de63f88d2e41ca0d0b0bc1dba4930cd2823b8e56
Moises <msira270997@gmail.com>
2023-08-07 15:50:08 -0400

    Se agregaron nuevos filtros a la clase de inscriptions

--- UnexpoIncrip/db/inscriptions.js
+++ UnexpoIncrip/db/inscriptions.js
@@ -7,6 +7,7 @@ class Inscriptions {
                             i.id,
                             s.name student,
                             ap.name period,
+                            ap.id period_id,
                             i.created_at,
                             i.detail,
                             i.status
@@ -21,7 +22,7 @@ class Inscriptions {
             let condition = "";
 
             if (criteria.id) {
-                condition += (condition ? " AND " : " WHERE ") + (" s.id = ? ")
+                condition += (condition ? " AND " : " WHERE ") + (" i.id = ? ")
                 param.push(criteria.id)
             }
 
@@ -48,6 +49,7 @@ class Inscriptions {
                             id: result[i].id,
                             student: result[i].student,
                             period: result[i].period,
+                            period_id: result[i].period_id,
                             created_at: result[i].created_at,
                             detail: result[i].detail,
                             status: result[i].status
@@ -90,6 +92,153 @@ class Inscriptions {
         }
     }
 
+    async filter_subject_period(criteria) {
+        try {
+            var sql = ` SELECT
+                            rd.id,
+                            aps.id a_period_subject_id,
+                            i.id inscription_id
+                        FROM
+                            registration_details rd
+                        INNER JOIN
+                            academic_period_subject aps ON rd.acad_per_subj_id = aps.id
+                        INNER JOIN
+                            inscriptions i ON rd.inscription_id = i.id
+                       `;
+            var param = [];
+            let condition = "";
+
+            if (criteria.id) {
+                condition += (condition ? " AND " : " WHERE ") + (" ps.id = ? ")
+                param.push(criteria.id)
+            }
+
+            if (criteria.period_id) {
+                condition += (condition ? " AND " : " WHERE ") + (" ps.period_id = ? ")
+                param.push(criteria.period_id)
+            }
+
+            if (criteria.teacher_id) {
+                condition += (condition ? " AND " : " WHERE ") + (" ps.teacher_id = ? ")
+                param.push(criteria.teacher_id)
+            }
+
+            if (criteria.subject_id) {
+                condition += (condition ? " AND " : " WHERE ") + (" ps.subject_id = ? ")
+                param.push(criteria.subject_id)
+            }
+
+            sql += condition;
+
+            if (criteria.limit)
+                sql += " limit " + criteria.limit
+
+            var con = await connect.createConnection();
+
+            var inscription_details = await new Promise((resolve, reject) => {
+                con.query(sql, param, function (err, result) {
+
+                    con.release(); // Importante siempre liberar la conexión despues de utilizarla.
+                    if (err) {
+                        reject(err)
+                        return;
+                    }
+
+                    var list = [];
+
+                    for (let i = 0; i < result.length; i++) {
+                        list.push({
+                            id: result[i].id,
+                            a_period_subject_id: result[i].a_period_subject_id,
+                            inscription_id: result[i].inscription_id
+                        })
+                    }
+
+                    resolve(list);
+                })
+
+            })
+
+            return inscription_details;
+
+        }
+        catch (ex) {
+            console.log(ex);
+        };
+
+    };
+
+    async filter_subject_section(criteria) {
+        try {
+            var sql = ` SELECT
+                            id,
+                            subject_id,
+                            section
+                        FROM
+                            academic_period_subject rd
+                       `;
+            var param = [];
+            let condition = "";
+
+            if (criteria.id) {
+                condition += (condition ? " AND " : " WHERE ") + (" ps.id = ? ")
+                param.push(criteria.id)
+            }
+
+            if (criteria.period_id) {
+                condition += (condition ? " AND " : " WHERE ") + (" ps.period_id = ? ")
+                param.push(criteria.period_id)
+            }
+
+            if (criteria.teacher_id) {
+                condition += (condition ? " AND " : " WHERE ") + (" ps.teacher_id = ? ")
+                param.push(criteria.teacher_id)
+            }
+
+            if (criteria.subject_id) {
+                condition += (condition ? " AND " : " WHERE ") + (" ps.subject_id = ? ")
+                param.push(criteria.subject_id)
+            }
+
+            sql += condition;
+
+            if (criteria.limit)
+                sql += " limit " + criteria.limit
+
+            var con = await connect.createConnection();
+
+            var subject_section = await new Promise((resolve, reject) => {
+                con.query(sql, param, function (err, result) {
+
+                    con.release(); // Importante siempre liberar la conexión despues de utilizarla.
+                    if (err) {
+                        reject(err)
+                        return;
+                    }
+
+                    var list = [];
+
+                    for (let i = 0; i < result.length; i++) {
+                        list.push({
+                            id: result[i].id,
+                            subject_id: result[i].subject_id,
+                            section: result[i].section
+                        })
+                    }
+
+                    resolve(list);
+                })
+
+            })
+
+            return subject_section;
+
+        }
+        catch (ex) {
+            console.log(ex);
+        };
+
+    };
 };
 
 module.exports.Inscriptions = Inscriptions;
