8838b77062fbbb11cfc1066b160f87be2e977ee9
Moises <msira270997@gmail.com>
2023-08-07 21:29:59 -0400

    Se agrego metodo post para agregar materias y un get para listar las materias inscritas

--- UnexpoIncrip/routes/inscriptions.js
+++ UnexpoIncrip/routes/inscriptions.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var inscriptionsDB = require('../db/inscriptions');
 var studentDB = require('../db/student');
 var periodDB = require('../db/period');
+var subjectDB = require('../db/subject')
+var teacherDB = require('../db/teacher')
 
 
 router.get('/', async function (req, res) { // req = request, res = response
@@ -44,17 +46,43 @@ router.post('/new', async function (req, res) {
 
 router.get('/subject/:inscription_id', async function (req, res) {
     var inscriptions_db = new inscriptionsDB.Inscriptions();
+    var subject_db = new subjectDB.Subject();
+    var teacher_db = new teacherDB.Teacher();
 
-    var inscription_details = await inscriptions_db.filter_subject_period({});
+    var inscription_details = await inscriptions_db.filter_subject_period({inscription_id: req.params.inscription_id});
     var inscriptions = await inscriptions_db.filter({id: req.params.inscription_id});
     let inscription_period = inscriptions[0].period_id;
-
+    let subject = [];
+    let teacher = [];
+    for(i = 0; i < inscription_details.length; i++){
+        subject.push({
+            subject: await subject_db.filter({id: inscription_details[i].subject_id})
+        })
+        teacher.push({
+            teacher: await teacher_db.filter({id: inscription_details[i].teacher_id})
+        })
+    }
     res.render('inscriptions/list_subjects', {
         title: "Listado de materias para inscribir",
         inscription_details: inscription_details,
         inscription_id: req.params.inscription_id,
-        inscription_period: inscription_period
+        inscription_period: inscription_period,
+        subject: subject,
+        teacher: teacher
     })
+})
+
+router.post('/subject/add', async function (req, res) {
+
+    var inscriptions_db = new inscriptionsDB.Inscriptions();
+
+    if (req.body.inscription_id && req.body.aps_id)
+        await inscriptions_db.add_subject({
+            inscription_id: req.body.inscription_id,
+            acad_per_subj_id: req.body.aps_id
+        });
+
+    res.send(JSON.stringify({ code: 0 }));
 })
 
 module.exports = router;
